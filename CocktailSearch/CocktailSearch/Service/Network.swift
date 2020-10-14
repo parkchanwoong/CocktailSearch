20008b4e096397f2bf4530694dca264d4e148d9b
[디렉션소프트]박찬웅 <chanwoong.park01@lotte.net>
2020-10-14 13:37:06 +0900

    불필요한 코드 삭제

--- CocktailSearch/CocktailSearch/Service/Network.swift
+++ CocktailSearch/CocktailSearch/Service/Network.swift
@@ -38,35 +38,4 @@ class Network {
             .map {try JSONDecoder().decode(RandomCocktail.self, from: $0.1)}
             .catchErrorJustReturn(nil)
     }
-
-    func getRandomCocktail() -> Observable<RandomCocktail> {
-        return Observable<RandomCocktail>.create({ observer -> Disposable in
-            let request = Network.alamofire.request(Api.random.url)
-                .validate(statusCode: 200..<300)
-                .responseData { (response) in
-                    switch response.result {
-                    case .success(let value):
-                        do {
-                            let res = try JSONDecoder().decode(RandomCocktail.self, from: value)
-                            observer.onNext(res)
-                            observer.onCompleted()
-                        } catch {
-                            observer.onError("\(error)" as! Error)
-                            print("에러")
-                        }
-                    case.failure(let error):
-                        observer.onError("\(error)" as! Error)
-                        print(error)
-                    }
-                }
-
-            return Disposables.create {
-                request.cancel()
-            }
-        })
-
-
-    }
-
-
 }
